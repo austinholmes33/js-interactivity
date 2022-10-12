9aecb9376313d6e54695a784526fb30863424b3e
Austin Holmes <austin.holmes4@gmail.com>
2022-10-12 16:27:32 -0600

    multiple error debugged, form submission still an issue

--- index.js
+++ index.js
@@ -4,30 +4,30 @@ const message = document.querySelector("#message");
 
 function addMovie(event) {
     event.preventDefault
-    const inputField = document.querySelector('input');
-    const movie = document.createElement('li');
-    const movieTitle = document.createElement('span');
+    let inputField = document.querySelector('input');
+    const movie = document.createElement("li");
+    const movieTitle = document.createElement("span");
     movieTitle.textContent = inputField;
     movie.appendChild(movieTitle);
-    const deleteBtn = createElement('button');
-    deleteBtn.textContent('X')
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = 'X'
     movie.appendChild(deleteBtn)
     const list = document.querySelector('ul');
     list.appendChild(movie);
-    inputField = '';
+    inputField = ('');
 }
 
 function deleteMovie(event) {
     event.target.parentNode.remove();
-    message.textContent('Movie Deleted!');
+    message.textContent = 'Movie Deleted!';
 }
 
 function crossOffMovie (event) {
     event.target.classList.toggle(".checked");
     if (event.target.classList.contains('.checked')) {
-        message.textContent('Movie Watched!')
+        message.textContent = 'Movie Watched!'
     } else {
-        message.textContent('Movie Added Back!')
+        message.textContent = 'Movie Added Back!'
     }
 }
 
