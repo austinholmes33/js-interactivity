0ae10dfb12047ac75258fcf14665c313e3c0b499
Austin Holmes <austin.holmes4@gmail.com>
2022-10-12 16:05:36 -0600

    core lab mostly done, debugging

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // console.log('hello')
-
+const message = document.querySelector("#message");
 
 
 function addMovie(event) {
@@ -9,11 +9,30 @@ function addMovie(event) {
     const movieTitle = document.createElement('span');
     movieTitle.textContent = inputField;
     movie.appendChild(movieTitle);
+    const deleteBtn = createElement('button');
+    deleteBtn.textContent('X')
+    movie.appendChild(deleteBtn)
     const list = document.querySelector('ul');
-    list.appendChild('movie');
+    list.appendChild(movie);
     inputField = '';
+}
+
+function deleteMovie(event) {
+    event.target.parentNode.remove();
+    message.textContent('Movie Deleted!');
+}
+
+function crossOffMovie (event) {
+    event.target.classList.toggle(".checked");
+    if (event.target.classList.contains('.checked')) {
+        message.textContent('Movie Watched!')
+    } else {
+        message.textContent('Movie Added Back!')
+    }
 }
 
 const form = document.querySelector('form');
 const button = document.querySelector('button');
 button.addEventListener('click', addMovie);
+button.addEventListener('click', deleteMovie);
+button.addEventListener('click', crossOffMovie);
